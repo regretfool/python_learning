ef70b8902f2b1ff95f464b9b72864132bcaef71c
regretfool <krzeminska.laura@gmail.com>
2025-10-21 18:21:55 +0100

    Learning variables

---
+++ variables.py
@@ -0,0 +1,21 @@
+x = 1
+print(x)
+x = 2
+print(x)
+y = x + 3
+print(y)
+
+name = "regretfool"
+language = "Python"
+# , adds always a space to the string
+print("My name is", name)
+print(name, "is learning", language)
+print(name, "wants to become a", language, "expert")
+
+# Challenge
+domain = "\bdatawithyon.com"  # Backspace to remove space
+website = "www."
+email = "\b@"
+print("info", email, domain)
+print("support", email, domain)
+print(website, domain)
