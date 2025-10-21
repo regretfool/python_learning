271d0923d019ecd22cc2d872e131d23addcf056b
regretfool <krzeminska.laura@gmail.com>
2025-10-21 17:58:31 +0100

    first commit

---
+++ hello.py
@@ -0,0 +1,14 @@
+print("Hi, this is my first python code")
+# This is a comment
+# 1. Create User Name
+# 2. Print Message to User
+# 3. Explode
+
+# Store the final exam score
+x = 10
+"""
+multi
+line
+string not comment!!!
+"""
+x = 9  # Final exam score
---
+++ print.py
@@ -0,0 +1,21 @@
+print("Hi Pyth'on")
+print('Hello P"y"thon')
+print("-----")
+print("|   |")
+print("|   |")
+print("-----")
+print("Special characters contain a \\ ")
+print("New\nline\n")
+print()
+print("")
+print("\n")
+print("Print \' and \" and \\ in text without breaking the string")
+print("Path: C:\\Users\\sasshin\\Documents")
+print("T\tab and Back\bspace")
+print("apples\t\tbananas\tpear")
+print("message1\tmessage2\n")
+# Challenge: make the layout in just one print \n\t for one line """ \t for block
+print("""Your Learning Path:
+      \t- Python Basics
+      \t- Data Engineering
+      \t- AI""")
